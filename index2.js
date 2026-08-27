aeb5291da8b4faec85f03aede5ad36f9df21a5ca
MOHD SAMEER <sameer.code07@gmail.com>
2026-08-27 23:30:17 +0530

    assignment-2 of js done

---
+++ index2.js
@@ -0,0 +1,204 @@
+// Loops and function assignment 
+
+// Section-1 basic loops 
+//part-A for loop 
+
+//print numbers 
+
+// for (i = 1; i <= 10; i++) {
+//     console.log(i);
+
+// }
+
+// print even number 
+
+// for(i=1 ; i<=10 ;i++){
+//     console.log(i*2);
+
+// }
+
+
+// Reverse counting 
+
+// for(i=10 ; i>=1; i--){
+//     console.log(i);
+
+// }
+
+// sum of num 
+// let sum = 0;
+// for (i = 1; i <= 10; i++) {
+//     sum+= i
+
+// }
+// console.log(sum);
+
+
+
+
+//Part-B while loop 
+
+// print number 
+// let i = 1;
+// while (i <= 10) {
+// console.log(i);
+//     i++;
+// }
+
+
+// sum of even number
+
+// let i = 2;
+// let sum = 0;
+
+// while (i <= 20) {
+//     sum += i;
+//     i += 2;
+// }
+// console.log(sum);
+
+
+// stop using loop using break;
+
+// for (i = 1; i <= 10; i++) {
+//     if (i >= 6) {
+//         break;
+//     }
+//     console.log(i);
+
+// }
+
+
+//skip a number using continue statement 
+
+// for(i=1;i<=10;i++){
+//     if(i==5){
+//         continue;
+//     }
+//     console.log(i);
+
+// }
+
+// let i = 1;
+// while (i <= 10) {
+
+//     if (i === 5) {
+//         i++;
+//         continue;
+//     }
+//     console.log(i);
+//     i++;
+
+
+// }
+
+
+//Section-2 Function
+
+//print greet msg 
+
+// function greetingMsg(greetMessage,userName){
+//     console.log(greetMessage,userName);
+
+// }
+// greetingMsg("hello","sameer");
+
+// add two number 
+
+// function sum(num1,num2){
+//     sum = num1+num2;
+//     console.log(sum);
+
+// }
+// sum(2,3);
+
+
+// check even or odd
+
+// function check(num) {
+//     if (num % 2 == 0) {
+//         console.log("even");
+
+//     } else {
+//         console.log("Odd");
+
+//     }
+// }
+// check(9);
+
+
+// square of a number
+
+// function square(num){
+//     console.log(`${num*num}`);
+
+// }
+// square(8);
+
+
+// largest of two number using  return 
+
+// function largest(num1,num2){
+//     if(num1>num2){
+//         return num1;
+
+//     }else{
+//         return num2;
+//     }
+// }
+
+// let num =largest(90,77);
+// console.log(num);
+
+
+// calculate price 
+
+// function clac(price , quantity){
+//     console.log(`${price}`*`${quantity}`);
+
+// }
+// clac(100,3);
+
+
+// section-2 function with loops 
+ 
+
+//print number 
+
+// function printNumber(n) {
+//     for (i = 1; i <= n; i++) {
+//         console.log(i);
+
+//     }
+// }
+// printNumber(5);
+
+//multiplication table 
+
+// function table(num){
+//     for(i=1 ; i<=10;i++){
+//         console.log(i*num);
+        
+//     }
+
+// }
+// table(8);
+
+//sum from 1 to N
+
+// function sum1toN(n){
+//     let sum = 0;
+//     for(i=1;i<=n;i++){
+//         sum+=i;
+//     }
+//     console.log(sum);
+    
+
+// }
+// sum1toN(10);
+
+
+// Finish
+
+
+
