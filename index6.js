bf301aed3cef1818a3a2aaed9ff343dcd783f94d
MOHD SAMEER <sameer.code07@gmail.com>
2026-09-04 23:33:30 +0530

    add assignment of map,filter & reduce

---
+++ index6.js
@@ -0,0 +1,155 @@
+// Map , filter and reduce assignment
+
+// Section-1 Map() & immutability
+
+// 1 . convert names to uppercase 1
+
+// const arr = ["laptop", "mobile", "headphones"];
+
+// // const arr1 = arr.map(function(product){
+// //     return product.toUpperCase()
+
+
+// // })
+
+// // arrow
+// const arr1 = arr.map(product => product.toUpperCase());
+
+// console.log(arr1);
+
+
+
+// 2. add a currency symbol to price
+
+// let arr = [100,200,300];
+
+// const withSymbol = arr.map( price => "$"+price);
+// console.log(withSymbol);
+
+
+// 3. Extract User name
+
+//  const students = [
+//     {
+//         name:"sameer",
+//         email:"sameer@google.com",
+//     },
+//     {
+//         name:"saif",
+//         email:"saif@yahoo.com",
+//     }
+//  ];
+
+//  const extractStudents = students.map( user =>user.name);
+// //  console.log(extractStudents);
+
+// // const extractStudents = students.map(function(UserName){
+// //     return UserName.name
+
+// // });
+// console.log(extractStudents);
+
+
+// 4. update price
+
+// let prices = [100,200,300];
+// const newPrices = prices.map(price => (price*1.1).toFixed(0) );
+// console.log(newPrices);
+
+
+// Update Object Data Immutably
+
+// let students = [
+//     { name: "Rahul", role: "student" },
+//     { name: "Priya", role: "student" }
+// ];
+
+// const newRole = students.map(user => ({ ...user, role: "developer" }));
+
+// console.log(newRole);
+
+
+// add a new property
+
+// let productSpec = [
+//     { name: "Laptop", price: 50000 },
+//     { name: "Mouse", price: 500 }
+//     ];
+
+// const newFeature = productSpec.map(stocks=>({...stocks, instocks:"true"}));
+// console.log(newFeature);
+// console.log(productSpec); //(... ki wajah se original arr bch gya)
+
+
+
+// Section-2 Map vs forEach
+
+// 7. Display Technologies Using forEach()
+
+// let arr1 = ["HTML", "CSS", "JavaScript"];
+// arr1.forEach(lang => console.log(lang));
+
+// 8 is same as 1
+
+
+// 9. Format User Names Using map()
+
+// let name = ["Rahul", "Priya", "Aman"];
+// const userName = name.map(user => "User:" + user);
+// console.log(userName);
+
+// Section-3 filter()
+
+// filter Available product
+
+// let products = [
+//     { name: "Laptop", inStock: true },
+//     { name: "Mouse", inStock: false }
+//     ] ;
+
+// const avail = products.filter(stock => stock.inStock)
+// console.log(avail);
+
+
+
+//Filter User by role
+
+// let candidate = [
+//     {
+//         name:"sameer",
+//         role:"developer"
+//     },
+//     {
+//         name:"anmol",
+//         role:"student"
+//     }
+// ];
+
+// const user = candidate.filter(select => select.role==="developer")
+// console.log(user);
+
+
+
+// . Filter Expensive Products
+
+// let products = [
+//     { name: "Mouse", price: 500 },
+//     { name: "Keyboard", price: 1500 }
+// ];
+
+// const Price = products.filter(value => value.price > 1000);
+// console.log(Price);
+
+
+
+// Filter Active Users
+// const users = [
+//     { name: "Rahul", isActive: true },
+//     { name: "Priya", isActive: false }
+// ]
+// const newUser = users.filter(function (username) {
+//     return username.isActive === true;
+// })
+// console.log(newUser);
+
+
